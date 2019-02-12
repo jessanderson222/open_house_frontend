1476ebe162b14781f548f27eb75af2f263dd48a5
jessanderson222 <jessanderson222@gmail.com>
2019-02-12 10:26:18 -0500

    sign in form w/o fetching agent, create account handleClick works

--- src/components/RealtorHome.js
+++ src/components/RealtorHome.js
@@ -1,9 +1,33 @@
 import React from "react";
 import { connect } from "react-redux";
+import RealtorSignInForm from "./RealtorSignInForm";
 
 class RealtorHome extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      createAccount: false
+    };
+  }
+
+  handleCreateAccount = e => {
+    this.setState({
+      createAccount: !this.state.createAccount
+    });
+  };
+
   render() {
-    return <h4>Home</h4>;
+    console.log(this.state);
+    return (
+      <div>
+        <RealtorSignInForm />
+        <br />
+        <p onClick={this.handleCreateAccount}>
+          Don't have an account? Create one here.
+        </p>
+      </div>
+    );
   }
 }
 
